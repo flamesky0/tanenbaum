59bcd1cd44a8adf957df0ce064481621a8115afa
flamesky <dueueigiffi@gmail.com>
2024-03-14 23:08:37 +0300

    made tinyusb to link normally

--- arm-none-eabi-gcc.cmake
+++ arm-none-eabi-gcc.cmake
@@ -7,7 +7,6 @@ set(CMAKE_C_EXTENTIONS OFF)
 
 set(CMAKE_C_FLAGS "-mcpu=cortex-m4 -mthumb\
 	-mfloat-abi=hard -mfpu=fpv4-sp-d16\
-        -fno-delete-null-pointer-checks\
 	-fdata-sections -ffunction-sections -Wall -O2"
 	CACHE STRING ""
 	)
